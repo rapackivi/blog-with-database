8734613981c3b6f1947e90fb27ff7cbbf90318c5
rapackivi <localit1981@gmail.com>
2020-04-26 15:40:17 +0300

    fixing app.js

--- app.js
+++ app.js
@@ -90,6 +90,6 @@ app.get("/posts/:postId", function(req, res){
   });
 });
 
-app.listen(env.process.PORT||3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
